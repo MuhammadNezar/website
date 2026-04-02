16cb76d96acc6bca595bc99776c8f6f53c1a53e4
Muhammad Nezar Sayyid Muslim <mnezar509@gmail.com>
2026-04-02 09:03:38 +0700

    Add files via upload

---
+++ scrift.js
@@ -0,0 +1,21 @@
+// animasi sederhana saat scroll
+const cards = document.querySelectorAll('.card');
+
+window.addEventListener('scroll', () => {
+    cards.forEach(card => {
+        const top = card.getBoundingClientRect().top;
+        const screenHeight = window.innerHeight;
+
+        if (top < screenHeight - 50) {
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+    });
+});
+
+// set awal animasi
+cards.forEach(card => {
+    card.style.opacity = "0";
+    card.style.transform = "translateY(50px)";
+    card.style.transition = "0.5s";
+});
