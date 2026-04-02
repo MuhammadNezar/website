9ead829e1a64276f2fd11141b121b4672b57a747
Muhammad Nezar Sayyid Muslim <mnezar509@gmail.com>
2026-04-02 09:18:06 +0700

    Update scrift.js

--- scrift.js
+++ scrift.js
@@ -1,6 +1,27 @@
-// animasi sederhana saat scroll
+// ======================
+// TOGGLE NAVBAR (MENU HP)
+// ======================
+const toggle = document.getElementById('menu-toggle');
+const nav = document.querySelector('.nav-links');
+
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+
+// ======================
+// ANIMASI CARD SAAT SCROLL
+// ======================
 const cards = document.querySelectorAll('.card');
 
+// set awal animasi
+cards.forEach(card => {
+    card.style.opacity = "0";
+    card.style.transform = "translateY(50px)";
+    card.style.transition = "0.5s ease";
+});
+
+// animasi saat scroll
 window.addEventListener('scroll', () => {
     cards.forEach(card => {
         const top = card.getBoundingClientRect().top;
@@ -11,11 +32,4 @@ window.addEventListener('scroll', () => {
             card.style.transform = "translateY(0)";
         }
     });
-});
-
-// set awal animasi
-cards.forEach(card => {
-    card.style.opacity = "0";
-    card.style.transform = "translateY(50px)";
-    card.style.transition = "0.5s";
 });
